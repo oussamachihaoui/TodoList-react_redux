bebe8d61a0602f87561529bd7d363a26f4ebc945
Oussama Chihaoui <chihaouioussama@gmail.com>
2024-07-09 11:27:47 +0100

    added done handler

--- src/Components/Todo.jsx
+++ src/Components/Todo.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useDispatch } from "react-redux";
-import { deleteTodo } from "../Redux/todoSlice";
+import { deleteTodo, doneTodo } from "../Redux/todoSlice";
 
 const Todo = ({ todo }) => {
   // consts
@@ -8,16 +8,34 @@ const Todo = ({ todo }) => {
 
   return (
     <div className="flex justify-between items-center w-1/2  p-5 border-b border-dotted  my-3 relative">
-      <button className="material-symbols-outlined p-2 bg-[#2a2a2a] rounded-full mr-5">
+      <button
+        className={
+          todo.done
+            ? "material-symbols-outlined p-2 bg-[#2a2a2a]  rounded-full mr-5 text-[#85bf6b] "
+            : "material-symbols-outlined p-2 bg-[#2a2a2a]  rounded-full mr-5"
+        }
+        onClick={() => {
+          dispatch(doneTodo(todo.id));
+        }}
+      >
         check
       </button>
-      <p className=" mr-auto">{todo.task}</p>
-      <div className="">
-        <button className="material-symbols-outlined mx-2 p-2 bg-[#2a2a2a] rounded-full ">
+
+      <p
+        className={
+          todo.done
+            ? " font-light mr-auto text-xl line-through decoration-[#85bf6b] text-[#666]"
+            : "mr-auto font-light text-xl"
+        }
+      >
+        {todo.task}
+      </p>
+      <div>
+        <button className="material-symbols-outlined mx-2 p-2 bg-[#2a2a2a] rounded-full hover:text-[#666] ">
           edit
         </button>
         <button
-          className="material-symbols-outlined p-2 bg-[#2a2a2a] rounded-full "
+          className="material-symbols-outlined p-2 bg-[#2a2a2a] rounded-full hover:text-[#666] "
           onClick={() => {
             dispatch(deleteTodo(todo.id));
           }}
@@ -32,6 +50,6 @@ const Todo = ({ todo }) => {
   );
 };
 
-// #85bf6b
+// line-through decoration-[#85bf6b] text-[#666] /
 
 export default Todo;
--- src/Redux/todoSlice.js
+++ src/Redux/todoSlice.js
@@ -28,8 +28,13 @@ const todo = createSlice({
     deleteTodo: (state, action) => {
       state.todos = state.todos.filter((todo) => todo.id !== action.payload);
     },
+    doneTodo: (state, action) => {
+      state.todos.map((todo) =>
+        action.payload === todo.id ? (todo.done = !todo.done) : todo
+      );
+    },
   },
 });
 
 export default todo.reducer;
-export const { addTodo, deleteTodo } = todo.actions;
+export const { addTodo, deleteTodo, doneTodo } = todo.actions;
